074acbd53993acd97333d1791833a43644723a4f
Raghav <raghavanand1869@gmail.com>
2024-05-02 15:33:45 -0400

    Added navbar template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+
+  );
+}
+
+export default App;
