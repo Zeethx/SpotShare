2509362b46c65a3afa82e52bbd291bb9e5220efb
Raghav <raghavanand1869@gmail.com>
2024-05-02 18:18:42 -0400

    Added a main container component and a pages folder to organise paths

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Header />
-      <Footer />
+        <Header />
+        <main>
+        <Outlet />
+        </main>
+        <Footer />
     </>
 
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from '../components/Container/Container'
+
+function Home() {
+  return (
+    <div>
+        <Container>
+            <h1 className='text-2xl font-bold text-center mt-4'>Home</h1>
+            <p className='text-center mt-4'>Welcome to the Home page</p>
+        </Container>
+    </div>
+  )
+}
+
+export default Home
