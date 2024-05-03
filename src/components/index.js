079817551576e2e9ed5d8e57daf478094b0324a3
HarshitOO7 <hbedi3@gmail.com>
2024-05-03 04:53:04 -0400

    Changes in Hero and form section

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import HeroSection from './HomePage/HeroSection'
+import Footer from './Footer/Footer'
+import Container from './Container/Container'
+import ParkingForm from './Form/ParkingForm'
+import InputField from './Form/InputField'
+// import Button from './Form/Button'
+import TestFooter from './Footer/TestFooter'
+
+export { 
+    HeroSection,
+    Footer,
+    Container,
+    ParkingForm,
+    InputField,
+    // Button,
+    TestFooter
+ }
+
